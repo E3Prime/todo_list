53010a66ba6b2fcc36efc464c5bcfd6d4c6150cb
E3Prime <primesoftware3@gmail.com>
2026-01-02 15:33:49 -0700

    core functionality of todo list added.

--- index.js
+++ index.js
@@ -12,23 +12,80 @@ function initialize() {
   const todoList = /** @type {HTMLElement} */ (document.getElementById('todoList'));
   const createTodoBtn = /** @type {HTMLButtonElement} */ (document.getElementById('createTodo'));
   /** @type {Todo[]} */
-  const todos = [{title: 'Learn C++'}, {title: 'Learn Rust'}];
+  const todos = [];
 
   /** @param {Event} e */
   const performAction = (e) => {
     const target = e.target;
-    if (!(target instanceof HTMLButtonElement)) return;
-    const [action, todoStr] = target.id.split('-');
-    const todoIndex = Number(todoStr);
-    // NOTE Left off here - implement action handlers
+    if (!(target instanceof HTMLInputElement || target instanceof HTMLButtonElement)) return;
+
+    const todoElem = /** @type {HTMLElement} */ (target.parentElement);
+    const checkboxElem = /** @type {HTMLInputElement} */ (todoElem.children[3]);
+    const checkboxElemId = Number(checkboxElem.id.slice(-1));
+    const [action, indexStr] = target.id.split('-');
+    const todoIndex = Number(indexStr);
+
+    if (action === 'update') updateTodo(todoElem, todoIndex);
+    else if (checkboxElem.checked || action === 'delete') deleteTodo(todoElem, checkboxElemId);
   };
 
-  const createTodo = () => {};
+  const createTodo = () => {
+    const inputElem = /** @type {HTMLInputElement} */ (createTodoBtn.previousElementSibling);
+    if (inputElem.value.replaceAll(' ', '').length < 2) return;
+    todos.push({title: inputElem.value});
+    inputElem.value = '';
+    saveTodos(todos);
+    renderTodos(todoList, todos);
+  };
 
-  const updateTodo = () => {};
+  const getTodos = () => {
+    const savedTodos = localStorage.getItem('todos');
+    if (!savedTodos) return;
+    const parsedTodos = /** @type {Todo[]} */ (JSON.parse(savedTodos));
+    parsedTodos.forEach((todo) => todos.push(todo));
+  };
 
-  const deleteTodo = () => {};
+  /**
+   * @param {HTMLElement} todoElem
+   * @param {number} todoIndex
+   */
+  const updateTodo = (todoElem, todoIndex) => {
+    const [labelElem, checkboxElem, inputElem] = /** @type {[HTMLLabelElement, HTMLInputElement, HTMLInputElement]} */ (Array.from(todoElem.children).slice(-3));
+    const actionImg = /** @type {HTMLImageElement} */ (todoElem.children[1].firstElementChild);
+    const action = /** @type {string} */ (actionImg.getAttribute('src')?.split('/').pop()?.split('.')[0]);
 
+    if (action === 'pen') {
+      labelElem.style.display = 'none';
+      checkboxElem.style.display = 'none';
+      actionImg.src = 'imgs/check.svg';
+      inputElem.value = todos[todoIndex].title;
+      inputElem.style.display = 'block';
+      inputElem.focus();
+    } else if (action === 'check') {
+      todos.splice(todoIndex, 1, {title: inputElem.value});
+      inputElem.value = '';
+      inputElem.style.display = 'none';
+      actionImg.src = 'imgs/pen.svg';
+      checkboxElem.style.display = 'block';
+      labelElem.style.display = 'block';
+      labelElem.textContent = todos[todoIndex].title;
+      saveTodos(todos);
+    }
+  };
+
+  /**
+   * @param {HTMLElement} todoElem
+   * @param {number} todoIndex
+   */
+  const deleteTodo = async (todoElem, todoIndex) => {
+    todoElem.classList.add('fade-out');
+    await sleep(1000);
+    todos.splice(todoIndex, 1);
+    saveTodos(todos);
+    renderTodos(todoList, todos);
+  };
+
+  getTodos();
   renderTodos(todoList, todos);
   todoList.addEventListener('click', performAction);
   createTodoBtn.addEventListener('click', createTodo);
@@ -44,18 +101,23 @@ function renderTodos(todoList, todos) {
   for (let i = 0; i < todos.length; ++i) {
     const todo = todos[i];
     const todoStructure = `
-      <div class="todo">
+      <div class="todo" id="todo-${i}">
         <button type="button" id="delete-${i}">
           <img src="imgs/trash.svg" alt="Delete Todo" />
         </button>
         <button type="button" id="update-${i}">
           <img src="imgs/pen.svg" alt="Update Todo" />
         </button>
-        <label for="todo-${i}">${todo.title}</label>
-        <input type="checkbox" id="todo-${i}" name="todo-${i}" />
+        <label for="todo-title-${i}">${todo.title}</label>
+        <input type="checkbox" id="todo-title-${i}" name="todo-title-${i}" />
         <input type="text" hidden />
       </div>
     `;
     todoList.innerHTML += todoStructure;
   }
+}
+
+/** @param {Todo[]} todos */
+function saveTodos(todos) {
+  localStorage.setItem('todos', JSON.stringify(todos));
 }
